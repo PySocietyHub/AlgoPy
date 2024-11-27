c07bc48919ae37212174fff2cd731d94cd4e8c47
yamilobt8 <yamilo12345678910@gmail.com>
2024-11-27 01:01:26 +0100

    Added Comments To Pascal_triangle

--- Algo S1/TP 1/pascal_triangle.py
+++ Algo S1/TP 1/pascal_triangle.py
@@ -1,23 +1,16 @@
-""" 
-1
-1 1
-1 2 1
-1 3 3 1
-1 4 6 4 1 
-"""
 n = int(input('entre the number of rows: '))
 
-pascal_triangle = [[1]]
+pascal_triangle = [[1]] # base dyal triangle
 
-for i in range(n):
-    triangle_row = [1]
-    for j in range(len(pascal_triangle[i]) - 1):
-        triangle_row.append(pascal_triangle[i][j] + pascal_triangle[i][j+1])
-    triangle_row.append(1)
-    pascal_triangle.append(triangle_row)
+for i in range(n): # ghandoro 3la 7sab ch7al mn ster 3tana l user
+    triangle_row = [1] # lbidaya dyal triangle
+    for j in range(len(pascal_triangle[i]) - 1): # ghandoro 3la ster li9bel bach mnno n9addo ster li3ndna
+        triangle_row.append(pascal_triangle[i][j] + pascal_triangle[i][j+1]) # ghanzido kola ra9em m3a li 7dah o nzidoh f ster dyalna
+    triangle_row.append(1) # ghadi nsddo ster b 1
+    pascal_triangle.append(triangle_row) # ghadi nzido ster 3la triangle
 
 
-for row in pascal_triangle:
-    for digit in row:
-        print(f'{digit} ', end='')
-    print()
+for row in pascal_triangle: # kolla ster mn triangle
+    for digit in row: # kolla ra9em f dak ster
+        print(f'{digit} ', end='') # ghanktboh ondiro espace
+    print() # mora mansaliw mn kolla ster ne9zo l lli tab3o
