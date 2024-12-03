e17f6b8b0fe0bddac7bc14e227bf8c4c3e54b3bc
yamilobt8 <yamilo12345678910@gmail.com>
2024-12-03 15:26:30 +0100

    Exo1 a/ b/

--- Algo S1/TP 2/exo1.py
+++ Algo S1/TP 2/exo1.py
@@ -1,7 +1,10 @@
+#a
+n = int(input('entrez un nombre: '))
+for i in range(1, 11):
+    print(f'{n} * {i} = {n * i}')
+    print('------------------------')
 
-    
 #b
-
 for i in range(1, 10):
     for j in range(1, 11):
         print(f'{i} * {j} = {j * i}')
