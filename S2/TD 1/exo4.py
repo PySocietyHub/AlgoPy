09f78d23621671e0654aab7df082f67019011c03
yamilobt8 <yamilo12345678910@gmail.com>
2025-03-20 12:49:15 +0000

    is_perfect_square

--- S2/TD 1/exo4.py
+++ S2/TD 1/exo4.py
@@ -4,12 +4,13 @@ en utilisant seulement les opérateurs de base, et renvoie sa racine dans le cas
 Indication :  
    n est un carré parfait sʼil existe un entier i tel que n =i×i
 """
+from math import isqrt
 
 def is_perfect_square(n):
     if n < 0: # squares can't be negative
         return False
     
-    root = n ** 0.5 
+    root = isqrt(n)
     return root * root == n
 
 print(is_perfect_square(25)) # True
