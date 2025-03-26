3abe2025ed3b9d3137e98861c35dc4d0e957bb21
yamilobt8 <yamilo12345678910@gmail.com>
2025-03-26 18:29:46 +0000

    recursive triangle

--- S2/TD 2/exo9.py
+++ S2/TD 2/exo9.py
@@ -1,8 +1,6 @@
-def triangle(n):
-    if n == 1:
-        print('*')
-        return
-    print('*' * n)
-    return triangle(n - 1)
+# Écrire une fonction récursive triangle qui prend un paramètre entier n et affichent un triangle :
 
-triangle(5)
+def triangle(n):
+    if n > 0:
+        print('*' * n)
+        return triangle(n - 1)
