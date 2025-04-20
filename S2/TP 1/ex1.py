ca472e0bfeb577ea3cbdfa1cb187072b24c7c55c
yamilobt8 <yamilo12345678910@gmail.com>
2025-04-20 17:52:21 +0100

    Power Function with default parameters and values

---
+++ S2/TP 1/ex1.py
@@ -0,0 +1,10 @@
+# 1 power Function
+def power(base, exponent=2):
+    if exponent < 0:
+        return float(base ** exponent)
+    return base ** exponent
+
+# 2 Test Cases
+print(power(5))
+print(power(base=3, exponent=4))
+print(power(2, exponent=5))
