472a13b0108f6e943e17b4fac25c2914176de586
Dexter <khalid.azaanoun.76@edu.uiz.ac.ma>
2025-04-30 10:55:12 +0100

    added a recursion  approach

--- S1/TD 3/easy-exo10.py
+++ S1/TD 3/easy-exo10.py
@@ -1,3 +1,12 @@
 n = int(input('entrez la valeur de N: '))
 for i in range(1, n + 1):
     print('*' * i)
+
+# recursive approach
+def triangle(n):
+    if n == 0:
+        return
+    triangle(n - 1)
+    print('*' * n)
+    
+triangle(n)
